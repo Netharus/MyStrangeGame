9b64efe24960ca0a90685b346be73b5f5568b1b9
Netharus <nekromant12331@gmail.com>
2021-10-16 00:24:34 +0300

    Bonus fix

--- Source.cpp
+++ Source.cpp
@@ -1,38 +1,26 @@
 #include<iostream>
 #include<cmath>
 #include<conio.h>
-#include<windows.h>
 #include<iomanip>
 #include<time.h>
 
 using namespace std;
 
-int movement_null(short x, short y,int score) {
-	for (int i = 0;i <= 90;i++) {
-		cout << "=";
-	}
-	cout << setw(20) << "Score:" << score;
-	cout << endl;
+int bonus_position(short* px_bonus, short* py_bonus, short x, short y) {
+	while (true) {
 
-	for (int i = 0;i <= 20;i++) {
+			(*px_bonus) = 2 + rand() % 86;
+			(*py_bonus) = rand() % 20;
 
-		if (i == y) {
-			cout << "||" << setw(x) << (char)(3) << setw(90 - x) << "||" << endl;
-		}
-		else
-		{
-			cout << "||" << setw(89) << "||" << endl;
+		if (((*px_bonus) != x && (*py_bonus) != y) || ((*px_bonus) != x && (*py_bonus)== y) || ((*px_bonus) == x && (*py_bonus) != y)) {
+
+			break;
 		}
 	}
-
-	for (int i = 0;i <= 90;i++) {
-		cout << "=";
-	}
-
-	return x, y;
+	return 0;
 }
 
-int movement(short x, short y, short x_bonus, short y_bonus,int *pscore,short *count_bonus) {
+int movement(short x, short y, short x_bonus, short y_bonus, int *pscore, short *pcount_bonus, short &px_bonus,short &py_bonus) {
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
@@ -76,46 +64,68 @@ int movement(short x, short y, short x_bonus, short y_bonus,int *pscore,short *c
 	}
 	if ((x == x_bonus) && (y == y_bonus)) {
 		(*pscore)++;
-		(*count_bonus) = 0;
 		system("cls");
+		bonus_position(&px_bonus, &py_bonus, x, y);
 		for (int i = 0;i <= 90;i++) {
 			cout << "=";
 		}
 		cout << setw(20) << "Score:" << *pscore;
 		cout << endl;
-		for (int i = 0;i <= 20;i++) {
+		if (y == py_bonus) {
+			for (int i = 0;i <= 20;i++) {
 
-			if (i == y) {
-				cout << "||" << setw(x) << (char)(3) << setw(89 - x) << "||" << endl;
-			}
-			else
-			{
-				cout << "||" << setw(89) << "||" << endl;
+				if (i == y && px_bonus < x) {
+					cout << "||" << setw(px_bonus) << (char)(4) << setw(x - (px_bonus)) << (char)(3) << setw(89 - (x)) << "||" << endl;
+				}
+				else
+					if (i == y && px_bonus > x) {
+						cout << "||" << setw(x) << (char)(3) << setw(px_bonus - (x)) << (char)(4) << setw(89 - (px_bonus)) << "||" << endl;
+					}
+					else
+					{
+						cout << "||" << setw(89) << "||" << endl;
+					}
 			}
 		}
+		else {
+			for (int i = 0;i <= 20;i++) {
 
+				if (py_bonus == i) {
+					cout << "||" << setw(px_bonus) << (char)(4) << setw(90 - (px_bonus + 1)) << "||" << endl;
+				}
+				else
+					if (i == y) {
+						cout << "||" << setw(x) << (char)(3) << setw(90 - (x + 1)) << "||" << endl;
+					}
+					else
+					{
+						cout << "||" << setw(89) << "||" << endl;
+					}
+			}
+		}
 		for (int i = 0;i <= 90;i++) {
 			cout << "=";
 		}
+		(*pcount_bonus)++;
 	}
 	else {
-		(*count_bonus)++;
+		(*pcount_bonus)++;
 	}
-	
-	return x,y,x_bonus,y_bonus;
+
+	return x, y, x_bonus, y_bonus;
 }
 
 int go() {
 	srand(time(NULL));
-	
-	int a,score=0;
+
+	int a, score = 0;
 	short x, y, bonus_chance, x_bonus, y_bonus, count_bonus = 0;
 
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
 	cout << setw(20) << "Score:" << score;
-	cout<< endl;
+	cout << endl;
 
 	for (int i = 0;i <= 20;i++) {
 
@@ -133,157 +143,71 @@ int go() {
 
 	x = 45;
 	y = 10;
-	
 
-	while(true) {
+
+	while (true) {
 		a = _getch();
+
 		if (count_bonus == 0) {
-			bonus_chance = 1 + rand() % 1;
-
-			while (true) {
-				if (bonus_chance <= 3) {
-
-					x_bonus = 2 + rand() % 86;
-					y_bonus = rand() % 20;
-				}
-				if ((x_bonus != x && y_bonus != y)||(x_bonus != x && y_bonus == y)||(x_bonus == x && y_bonus != y)) {
-
-					break;
-				}
-			}
+			bonus_position(&x_bonus,&y_bonus,x,y);
+			
 		}
 
 		switch (a)
 		{
 		case 97://a
-				
+
 			if (x == 1) {
 				break;
-			}
+			}		         
 			
-			switch (bonus_chance) {
-			case 1://with bonus
+			system("cls");
 
-					system("cls");
+				x--;
+				movement(x, y, x_bonus, y_bonus, &score, &count_bonus,x_bonus,y_bonus);
 
-					x--;
-					movement(x,y,x_bonus,y_bonus,&score,&count_bonus);
-
-				break;//with bonus
-
-			case 2://without bonus
-
-					system("cls");
-
-					x--;
-
-					movement_null(x, y,score);
-				break;//without bonus
-			default:;
-				break;
-			}
-
-		break;// a
+			break;// a
 
 		case 100://d
-			
+
 			if (x == 87) {
 				break;
 			}
 
-			switch (bonus_chance) {
-			case 1://with bonus
-
-				system("cls");
+			system("cls");
 
 				x++;
 
-				movement(x, y, x_bonus, y_bonus,&score,&count_bonus);
-
-
-
-				break;//with bonus
-
-			case 2://without bonus
-
-				system("cls");
-
-				x++;
-
-				movement_null(x, y,score);
-
-				break;//without bonus
-			default:;
-				break;
-			}
-
+				movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
+							   				 			
 			break;//d
 
 		case 119://w
-			
+
 			if (y == 0) {
 				break;
 			}
 
-			switch (bonus_chance) {
-			case 1://with bonus
+			system("cls");
 
-				system("cls");
+			y--;
 
-				y--;
-
-				movement(x, y, x_bonus, y_bonus,&score,&count_bonus);
-				
-
-
-				break;//with bonus
-
-
-			case 2://without bonus
-
-				system("cls");
-
-				y--;
-
-				movement_null(x, y,score);
-
-				break;//without bonus
-			default:;
-				break;
-			}
-
+			movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
+					   			 	
 			break;//w
 
 		case 115:
+
 			if (y == 20) {
 				break;
 			}
-			switch (bonus_chance) {
-			case 1://with bonus
+			
+			system("cls");
 
-				system("cls");
+			y++;
 
-				y++;
-
-				movement(x, y, x_bonus, y_bonus,&score,&count_bonus);
-
-
-
-				break;//with bonus
-
-			case 2://without bonus
-
-				system("cls");
-
-				y++;
-
-				movement_null(x, y,score);
-
-				break;//without bonus
-			default:;
-				break;
-			}
-
+			movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
+					   			 		  		  
 			break;
 		}
 
