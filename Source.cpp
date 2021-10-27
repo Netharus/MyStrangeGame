3c1d23b321caf2b03058d82afd61c32f4b752251
Netharus <nekromant12331@gmail.com>
2021-10-27 09:26:08 +0300

    Menu update

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,15 @@
 
 using namespace std;
 
-int bonus_position(short* px_bonus, short* py_bonus, short x, short y) {
+void control() {
+	system("cls");
+	cout << "a-left\nd-right\nw-up\ns-down" << endl;
+	cout << "For exit press any buttom" << endl;
+		_getch();
+
+}
+void menu(bool* pflag_exit);
+int position_object(short* px_bonus, short* py_bonus, short x, short y) {
 	while (true) {
 
 			(*px_bonus) = 2 + rand() % 86;
@@ -19,23 +27,22 @@ int bonus_position(short* px_bonus, short* py_bonus, short x, short y) {
 	}
 	return 0;
 }
-
-int movement(short x, short y, short x_bonus, short y_bonus, int *pscore, short *pcount_bonus, short &px_bonus,short &py_bonus) {
+int movement_person(short x, short y, short *px_bonus, short *py_bonus, int* pscore, short* pcount_bonus) {
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
 	cout << setw(20) << "Score:" << *pscore;
 	cout << endl;
 
-	if (y == y_bonus) {
+	if (y == *py_bonus) {
 		for (int i = 0;i <= 20;i++) {
 
-			if (i == y && x_bonus < x) {
-				cout << "||" << setw(x_bonus) << (char)(4) << setw(x - (x_bonus)) << (char)(3) << setw(89 - (x)) << "||" << endl;
+			if (i == y && *px_bonus < x) {
+				cout << "||" << setw(*px_bonus) << (char)(4) << setw(x - (*px_bonus)) << (char)(3) << setw(89 - (x)) << "||" << endl;
 			}
 			else
-				if (i == y && x_bonus > x) {
-					cout << "||" << setw(x) << (char)(3) << setw(x_bonus - (x)) << (char)(4) << setw(89 - (x_bonus)) << "||" << endl;
+				if (i == y && *px_bonus > x) {
+					cout << "||" << setw(x) << (char)(3) << setw(*px_bonus - (x)) << (char)(4) << setw(89 - (*px_bonus)) << "||" << endl;
 				}
 				else
 				{
@@ -46,8 +53,8 @@ int movement(short x, short y, short x_bonus, short y_bonus, int *pscore, short 
 	else {
 		for (int i = 0;i <= 20;i++) {
 
-			if (y_bonus == i) {
-				cout << "||" << setw(x_bonus) << (char)(4) << setw(90 - (x_bonus + 1)) << "||" << endl;
+			if (*py_bonus == i) {
+				cout << "||" << setw(*px_bonus) << (char)(4) << setw(90 - (*px_bonus + 1)) << "||" << endl;
 			}
 			else
 				if (i == y) {
@@ -62,65 +69,36 @@ int movement(short x, short y, short x_bonus, short y_bonus, int *pscore, short 
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
-	if ((x == x_bonus) && (y == y_bonus)) {
+	return 0;
+}
+int movement(short x, short y, int *pscore, short *pcount_bonus, short *px_bonus,short *py_bonus) {
+	
+	movement_person(x, y, px_bonus, py_bonus, pscore, pcount_bonus);
+
+	if ((x == *px_bonus) && (y == *py_bonus)) {
 		(*pscore)++;
+
 		system("cls");
-		bonus_position(&px_bonus, &py_bonus, x, y);
-		for (int i = 0;i <= 90;i++) {
-			cout << "=";
-		}
-		cout << setw(20) << "Score:" << *pscore;
-		cout << endl;
-		if (y == py_bonus) {
-			for (int i = 0;i <= 20;i++) {
-
-				if (i == y && px_bonus < x) {
-					cout << "||" << setw(px_bonus) << (char)(4) << setw(x - (px_bonus)) << (char)(3) << setw(89 - (x)) << "||" << endl;
-				}
-				else
-					if (i == y && px_bonus > x) {
-						cout << "||" << setw(x) << (char)(3) << setw(px_bonus - (x)) << (char)(4) << setw(89 - (px_bonus)) << "||" << endl;
-					}
-					else
-					{
-						cout << "||" << setw(89) << "||" << endl;
-					}
-			}
-		}
-		else {
-			for (int i = 0;i <= 20;i++) {
-
-				if (py_bonus == i) {
-					cout << "||" << setw(px_bonus) << (char)(4) << setw(90 - (px_bonus + 1)) << "||" << endl;
-				}
-				else
-					if (i == y) {
-						cout << "||" << setw(x) << (char)(3) << setw(90 - (x + 1)) << "||" << endl;
-					}
-					else
-					{
-						cout << "||" << setw(89) << "||" << endl;
-					}
-			}
-		}
-		for (int i = 0;i <= 90;i++) {
-			cout << "=";
-		}
+		
+		position_object(px_bonus, py_bonus, x, y);
+		
+		movement_person(x, y, px_bonus, py_bonus, pscore, pcount_bonus);
+		
 		(*pcount_bonus)++;
 	}
 	else {
 		(*pcount_bonus)++;
 	}
 
-	return x, y, x_bonus, y_bonus;
+	return x, y;
 }
-
-int go() {
+int go(bool *pflag_exit,bool *pflag_menu) {
 	srand(time(NULL));
 
 	int a, score = 0;
-	short x, y, bonus_chance, x_bonus, y_bonus, count_bonus = 0;
+	short x=45, y=10, x_bonus, y_bonus, count_bonus = 0,x_enemy,y_enemy,count_enemy;
 
+	system("cls");
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
@@ -140,16 +118,12 @@ int go() {
 	for (int i = 0;i <= 90;i++) {
 		cout << "=";
 	}
-
-	x = 45;
-	y = 10;
-
-
+	
 	while (true) {
 		a = _getch();
 
 		if (count_bonus == 0) {
-			bonus_position(&x_bonus,&y_bonus,x,y);
+			position_object(&x_bonus,&y_bonus,x,y);
 			
 		}
 
@@ -164,7 +138,7 @@ int go() {
 			system("cls");
 
 				x--;
-				movement(x, y, x_bonus, y_bonus, &score, &count_bonus,x_bonus,y_bonus);
+				movement(x, y,  &score, &count_bonus,&x_bonus,&y_bonus);
 
 			break;// a
 
@@ -178,8 +152,8 @@ int go() {
 
 				x++;
 
-				movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
-							   				 			
+				movement(x, y, &score, &count_bonus, &x_bonus, &y_bonus);
+
 			break;//d
 
 		case 119://w
@@ -192,7 +166,7 @@ int go() {
 
 			y--;
 
-			movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
+			movement(x, y,  &score, &count_bonus, &x_bonus, &y_bonus);
 					   			 	
 			break;//w
 
@@ -206,16 +180,130 @@ int go() {
 
 			y++;
 
-			movement(x, y, x_bonus, y_bonus, &score, &count_bonus, x_bonus, y_bonus);
+			movement(x, y,  &score, &count_bonus, &x_bonus, &y_bonus);
 					   			 		  		  
 			break;
+		case 27:
+			system("cls");
+			*pflag_menu = false;
+			menu(pflag_exit);
+			break;
 		}
+		
+		
+	if (*pflag_exit == true||a==27) {
+		break;
+	}
+	}
+	if (*pflag_exit == true||a==27) {
+		return 0;
+	}
+}
+void menu(bool *pflag_exit) {
+	bool flag=true,flag_end=false,flag_control=false,flag_control_leave=false;
+	short choice, buff_choice;
+	char a=62, b=0, c=0;
 
+	cout << setw(8) << "Menu" << endl;
+	cout << ">1) New game\n2) Control\n3) Exit" << endl;
+	
+
+	while (true) {
+		if (flag_control_leave == true) {
+			system("cls");
+			cout << setw(8) << "Menu" << endl;
+			cout << "1) New game\n>2) Control\n3) Exit" << endl;
+			flag_control_leave = false;
+		}
+		if (flag == true) {
+			buff_choice = 1;
+			flag = false;
+		}
+		flag = false;
+		
+
+			for (int i = 0;i <= 2;i++) {
+				choice = _getch();
+				if (choice == 80) {
+					buff_choice++;
+					break;
+				}
+				else if (choice == 72) {
+					buff_choice--;
+					break;
+				}
+				else if (choice == 13) {
+					if (buff_choice == 1) {
+						flag_end = true;
+						break;
+					}
+					if (buff_choice == 2) {
+						flag_control = true;
+						break;
+					}
+					if (buff_choice == 3) {
+						*pflag_exit = true;
+						flag_end = true;
+						break;
+					}
+				}
+			}
+		
+	
+		
+		if (flag_control == true) {
+			control();
+			flag_control=false;
+			flag_control_leave=true;
+			
+		}
+		else {
+
+			if (flag_end == true) {
+				break;
+			}
+			if (buff_choice > 3) {
+				buff_choice = 3;
+			}
+			if (buff_choice < 1) {
+				buff_choice = 1;
+			}
+			switch (buff_choice) {
+			case 1: a = 62;
+				b = 0;
+				c = 0;
+				break;
+			case 2: a = 0;
+				b = 62;
+				c = 0;
+				break;
+			case 3: a = 0;
+				b = 0;
+				c = 62;
+				break;
+			}
+			system("cls");
+			cout << setw(8) << "Menu\n" << a << "1) New game\n" << b << "2) Control\n" << c << "3) Exit" << endl;
+		}
 	}
 }
 int main() {
-	go();
+	bool flag_exit=false,flag_menu=true;
 
-	cout << _getch();
+	while (flag_exit == false) {
+		if (flag_menu == true) {
+			menu(&flag_exit);
+		}
+		if (flag_exit == true) {
+			return 0;
+		}
+		
+		go(&flag_exit,&flag_menu);
+		if (flag_exit == true) {
+			return 0;
+		}
+	}
+
+	
 	return 0;
 }
